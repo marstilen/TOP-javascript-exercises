82bd398c00ab27ccba4c91fe0448f004be84768b
marstilen <tilen.mars@proton.me>
2025-11-05 22:27:17 +0100

    Solve "09_sumAll"

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
-};
+const sumAll = function(lowestValue, highestValue) {
+    let sum = 0;
+    if(lowestValue < 0 || highestValue < 0 || lowestValue === NaN || highestValue === NaN || lowestValue%1 != 0 || typeof lowestValue != "number" || typeof highestValue != "number"){
+        return "ERROR";
+    }
+    else if(lowestValue > highestValue)
+    {
+        let temp = lowestValue; 
+        lowestValue = highestValue;
+        highestValue = temp;
+    }
+    while(lowestValue <= highestValue){
+        sum += lowestValue;
+        lowestValue++;
+}
+return sum};
 
 // Do not edit below this line
 module.exports = sumAll;
