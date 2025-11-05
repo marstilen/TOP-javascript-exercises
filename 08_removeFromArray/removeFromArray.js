61046ad78729e6b2f34b6f8b564de3a185767c58
marstilen <tilen.mars@proton.me>
2025-11-05 21:54:01 +0100

    Solve "08_removeFromArray"

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, elementToRemove) {
+    for (let i = 1; i < arguments.length; i++) {    
+        arr = arr.filter(element => element !== arguments[i]);
+    }
+    return arr;
 };
 
 // Do not edit below this line
