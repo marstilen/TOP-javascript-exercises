0be929cbb7f69cce3b696895330bbbc6afcb0451
marstilen <tilen.mars@proton.me>
2025-11-07 07:10:36 +0100

    Solve "11_tempConversion"

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahr) {
+  return Number(((tempInFahr-32) * 5/9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCel) {
+  return Number((tempInCel*9/5+32).toFixed(1))
 };
 
 // Do not edit below this line
