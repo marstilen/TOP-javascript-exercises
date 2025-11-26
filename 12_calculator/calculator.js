28723d4d4357a0c2216f1bfd3c9c23764a4d884e
marstilen <tilen.mars@proton.me>
2025-11-26 18:36:19 +0100

    Solve "12_calculator"

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  const initialValue = 0;
+	return arr.reduce((total, element) => total + element, initialValue)
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  const initialValue = 1;
+  return a.reduce((total, element) => total * element, initialValue)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(arr) {
+  let total = 1;
+	for (let i = 1; i <= arr; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
