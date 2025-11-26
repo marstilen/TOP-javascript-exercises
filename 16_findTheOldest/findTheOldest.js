1ad8f826d48ae41f3b56c249d44c97267894cb78
marstilen <tilen.mars@proton.me>
2025-11-26 19:48:17 +0100

    Solve "16_findTheOldest"

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+function calculateAge(person) {
+    const dateToday = new Date();
+    if (!person.yearOfDeath) {
+        return dateToday.getFullYear() - person.yearOfBirth; 
+    }
+    else return person.yearOfDeath - person.yearOfBirth; 
+}
 
+const findTheOldest = function(people) {
+ let peopleSorted =  people.sort((a,b) => calculateAge(a) - calculateAge(b));
+ return peopleSorted[peopleSorted.length-1]; 
 };
 
 // Do not edit below this line
