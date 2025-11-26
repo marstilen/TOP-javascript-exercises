c0fa56b14e714de63bf7be79e45669de0107a515
marstilen <tilen.mars@proton.me>
2025-11-26 18:54:16 +0100

    Solve "13_palindromes"

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toLowerCase();
+    word = word.replaceAll(" ", "")
+    word = word.replace(/[^a-zA-Z0-9\s]/g, '');
+    let wordBackwards = [];
+    let i = word.length - 1; 
+    while(i >= 0){
+        wordBackwards.push(word[i]);
+        i--;
+ }
+    return wordBackwards.join("") == word;
 };
 
 // Do not edit below this line
